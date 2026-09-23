bbb0e93979156d441b0a1cf4d8031c252dee6b4e
HeyItsManali <manalibhamble@gmail.com>
2026-09-23 10:56:49 +0530

    Update database connection credentials and clean code

--- Backend/server2.js
+++ Backend/server2.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 const db=mysql.createConnection({
 host:'localhost',
-user:'root',
-password:'MySQL@2026@31',
+user:'event_sphere',
+password:'Shruti@11',
 database:'event_sphere'
 });
 
